c8d20c11658bef297e9799029b4bdc71a2dcde12
Safi Ullah <safiullah445@outlook.com>
2025-04-14 13:47:59 +0500

    time spans added

--- selenium_test.py
+++ selenium_test.py
@@ -20,27 +20,28 @@ try:
         EC.presence_of_element_located((By.XPATH, '//input[@id="name"]'))
     )
     first_name_input.clear()
-    first_name_input.send_keys("John")
-
+    first_name_input.send_keys("Test")
+    time.sleep(2)
     # Wait for the second input (email)
     last_name_input = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//input[@id="email"]'))
     )
     last_name_input.clear()
-    last_name_input.send_keys("Doe@gmail.com")
-
+    last_name_input.send_keys("test@gmail.com")
+    time.sleep(2)
     subject_input = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//input[@id="subject"]'))
     )
     subject_input.clear()
     subject_input.send_keys("This is a test")
-
+    time.sleep(2)
     Message_input = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//textarea[@id="message"]'))
     )
     Message_input.clear()
     Message_input.send_keys("This is a test message send by the selenium to test the webdriver and selenium installation")
-
+    
+    time.sleep(3)
 
     # Find and click the submit button (here it's a regular button)
     submit_btn = driver.find_element(By.XPATH, '//button[text()="Send Message"]')
